45e99141922b234cefc94d991df2ddf9aa3143bb
Summon Yang <SummonYang1991@gmail.com>
2015-05-05 11:47:17 +0800

    Create max_temperature.sh file

--- chapter02/sh/max_temperature.sh
+++ chapter02/sh/max_temperature.sh
@@ -1,8 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
-for year in ../../data/193[0-9]
+for year in ../../data/all/*
 do
-    echo -ne `basename $year/ .tar`"\t"
-    
-
+    echo -ne `basename $year .gz`"\t"
+    gunzip -c $year | awk ' { temp = substr($0, 88, 5) + 0;
+                              q = substr($0, 93, 1);
+                              if (temp != 9999 && q ~/[01459]/ && temp > max)
+                                  max = temp; }
+                            END {print max}'
 done
